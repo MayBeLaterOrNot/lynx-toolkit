f147f0d440c5c59f20e67dd07415b0553655d7b7
Oystein Bjorke <oystein.bjorke@gmail.com>
2012-08-24 14:38:19 +0200

    Added FtpUpload

---
+++ Source/FtpUpload/Program.cs
@@ -0,0 +1,94 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="Program.cs" company="LynxToolkit">
+//     Copyright © LynxToolkit. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+using System;
+using System.IO;
+
+namespace FtpUpload
+{
+    using System.Net;
+    using System.Reflection;
+
+    // Uploads a single file to FTP using binary/passive mode
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("FtpUpload {0}", Assembly.GetExecutingAssembly().GetName().Version);
+            if (args.Length < 5)
+            {
+                Console.WriteLine("Missing arguments.");
+                Console.WriteLine("Syntax: [host] [username] [password] [localfile] [remotefile]");
+                return;
+            }
+
+            var host = args[0];
+            var userName = args[1];
+            var password = args[2];
+            var localFile = args[3];
+            var remoteFile = args[4];
+            Console.WriteLine("{0} => {1}/{2}", localFile, host, remoteFile);
+            var length = UploadFile(host, userName, password, remoteFile, localFile, 1024, (x, y) =>
+            {
+                Console.Write("#");
+                return true;
+            });
+            Console.WriteLine("\n{0} bytes uploaded.", length);
+
+        }
+
+
+
+        public static long UploadFile(string host, string userName, string password, string remoteFile, string localFile, int bufferSize = 2048, Func<long, long, bool> callback = null)
+        {
+            try
+            {
+
+                var request = (FtpWebRequest)WebRequest.Create("ftp://" + host + "/" + remoteFile);
+                request.Credentials = new NetworkCredential(userName, password);
+                request.UseBinary = true;
+                request.UsePassive = true;
+                request.KeepAlive = true;
+                request.Method = WebRequestMethods.Ftp.UploadFile;
+
+                using (var requestStream = request.GetRequestStream())
+                {
+                    using (var localFileStream = new FileStream(localFile, FileMode.Open))
+                    {
+                        var byteBuffer = new byte[bufferSize];
+                        long total = 0;
+                        while (true)
+                        {
+                            // Read from local stream
+                            var bytes = localFileStream.Read(byteBuffer, 0, bufferSize);
+                            if (bytes == 0) break;
+
+                            // Write to request stream
+                            requestStream.Write(byteBuffer, 0, bytes);
+                            total += bytes;
+                            if (callback != null)
+                            {
+                                var cont = callback(localFileStream.Length, total);
+                                if (!cont)
+                                {
+                                    break;
+                                }
+                            }
+                        }
+                        return total;
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return -1;
+            }
+        }
+
+    }
+}
