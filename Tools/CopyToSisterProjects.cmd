346950e3314b2fe096abf878c4e0c198c711b6e3
Oystein Bjorke <oystein.bjorke@gmail.com>
2012-10-11 12:11:46 +0200

    Added copy script

---
+++ Tools/CopyToSisterProjects.cmd
@@ -0,0 +1,8 @@
+copy ..\Output\*.* ..\..\HelixToolkit\Tools\Lynx
+copy ..\Output\*.* ..\..\OxyPlot\Tools\Lynx
+copy ..\Output\*.* ..\..\PropertyTools\Tools\Lynx
+copy ..\Output\*.* ..\..\Units\Tools\Lynx
+
+copy ..\Output\*.* ..\..\UsageStats\Tools\Lynx
+copy ..\Output\*.* ..\..\Glissando\Tools\Lynx
+
