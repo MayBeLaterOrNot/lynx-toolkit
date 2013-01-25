f7bf9a311fde7fdbbf7d4875c8345500bd3000ef
Oystein Bjorke <oystein.bjorke@gmail.com>
2013-01-25 23:05:09 +0100

    WikiParserBase cleanup

--- Source/LynxToolkit.Documents/Text/Parsers/WikiParserBase.cs
+++ Source/LynxToolkit.Documents/Text/Parsers/WikiParserBase.cs
@@ -1,18 +1,67 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text.RegularExpressions;
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="WikiParserBase.cs" company="Lynx">
+//   The MIT License (MIT)
+//   
+//   Copyright (c) 2012 Oystein Bjorke
+//   
+//   Permission is hereby granted, free of charge, to any person obtaining a
+//   copy of this software and associated documentation files (the
+//   "Software"), to deal in the Software without restriction, including
+//   without limitation the rights to use, copy, modify, merge, publish,
+//   distribute, sublicense, and/or sell copies of the Software, and to
+//   permit persons to whom the Software is furnished to do so, subject to
+//   the following conditions:
+//   
+//   The above copyright notice and this permission notice shall be included
+//   in all copies or substantial portions of the Software.
+//   
+//   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+//   OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+//   MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+//   IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+//   CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+//   TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+//   SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
 
 namespace LynxToolkit.Documents
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Text.RegularExpressions;
+
     public abstract class WikiParserBase : DocumentParser
     {
-        public string BaseDirectory { get; set; }
+        private static readonly char[] newlineChars = new[] { '\r', '\n' };
+
+        protected Regex BlocksExpression;
+
+        protected Regex DirectivesExpression;
+
+        protected Regex InlineExpression;
+
+        protected Regex ListItemExpression;
 
         protected Regex TableRowExpression;
-        protected Regex ListItemExpression;
-        protected Regex DirectivesExpression;
-        protected Regex BlocksExpression;
-        protected Regex InlineExpression;
+
+        private Regex NewlineWhitespaceExpression = new Regex(@"\s*\r?\n\r?\s*", RegexOptions.Compiled);
+
+        public string BaseDirectory { get; set; }
+
+        protected virtual void AddHeader(string text, int level)
+        {
+            var h = new Header { Level = level };
+            this.ParseInlines(text, h.Content);
+            this.doc.Blocks.Add(h);
+        }
+
+        protected virtual void AddParagraph(string text)
+        {
+            var pa = new Paragraph();
+            this.ParseInlines(text, pa.Content);
+            this.doc.Blocks.Add(pa);
+        }
 
         protected void ParseCore(string text)
         {
@@ -20,67 +69,78 @@ namespace LynxToolkit.Documents
             text = text.Replace("\r\n", "\n");
 
             int index = 0;
-            foreach (Match match in BlocksExpression.Matches(text))
+            foreach (Match match in this.BlocksExpression.Matches(text))
             {
                 if (match.Index > index)
                 {
                     var s = text.Substring(index, match.Index - index);
                     if (!string.IsNullOrWhiteSpace(s))
-                        AddParagraph(s);
+                    {
+                        this.AddParagraph(s);
+                    }
                 }
+
                 index = match.Index + match.Length;
 
                 var h1 = match.Groups["h1"];
                 if (h1.Success)
                 {
-                    AddHeader(h1.Value, 1);
+                    this.AddHeader(h1.Value, 1);
                     continue;
                 }
+
                 var h2 = match.Groups["h2"];
                 if (h2.Success)
                 {
-                    AddHeader(h2.Value, 2);
+                    this.AddHeader(h2.Value, 2);
                     continue;
                 }
+
                 var h = match.Groups["h"];
                 if (h.Success)
                 {
-                    AddHeader(h.Value, match.Groups["hlevel"].Value.Length);
+                    this.AddHeader(h.Value, match.Groups["hlevel"].Value.Length);
                     continue;
                 }
+
                 var table = match.Groups["table"];
                 if (table.Success)
                 {
-                    AddTable(table.Value);
+                    this.AddTable(table.Value);
                     continue;
                 }
+
                 var quote = match.Groups["quote"];
                 if (quote.Success)
                 {
-                    AddQuote(quote.Value);
+                    this.AddQuote(quote.Value);
                     continue;
                 }
+
                 var ul = match.Groups["ul"];
                 if (ul.Success)
                 {
-                    AddList(ul.Value);
+                    this.AddList(ul.Value);
                     continue;
                 }
+
                 var ol = match.Groups["ol"];
                 if (ol.Success)
                 {
-                    AddList(ol.Value);
+                    this.AddList(ol.Value);
                     continue;
                 }
+
                 var code = match.Groups["code"];
                 if (code.Success)
                 {
-                    AddCodeBlock(code.Value, match.Groups["codelanguage"].Value);
+                    this.AddCodeBlock(code.Value, match.Groups["codelanguage"].Value);
                     continue;
                 }
+
                 if (match.Groups["hr"].Success)
                 {
-                    doc.Blocks.Add(new HorizontalRuler());
+                    this.doc.Blocks.Add(new HorizontalRuler());
                     continue;
                 }
             }
@@ -89,32 +149,52 @@ namespace LynxToolkit.Documents
             {
                 var s = text.Substring(index);
                 if (!string.IsNullOrWhiteSpace(s))
-                    AddParagraph(s);
+                {
+                    this.AddParagraph(s);
+                }
             }
         }
 
         protected virtual Language ParseLanguage(string language)
         {
-            if (string.Equals(language, "cs", StringComparison.OrdinalIgnoreCase)) return Language.Cs;
-            if (string.Equals(language, "c#", StringComparison.OrdinalIgnoreCase)) return Language.Cs;
-            if (string.Equals(language, "js", StringComparison.OrdinalIgnoreCase)) return Language.Js;
-            if (string.Equals(language, "xml", StringComparison.OrdinalIgnoreCase)) return Language.Xml;
-            if (string.Equals(language, "xaml", StringComparison.OrdinalIgnoreCase)) return Language.Xml;
+            if (string.Equals(language, "cs", StringComparison.OrdinalIgnoreCase))
+            {
+                return Language.Cs;
+            }
+
+            if (string.Equals(language, "c#", StringComparison.OrdinalIgnoreCase))
+            {
+                return Language.Cs;
+            }
+
+            if (string.Equals(language, "js", StringComparison.OrdinalIgnoreCase))
+            {
+                return Language.Js;
+            }
+
+            if (string.Equals(language, "xml", StringComparison.OrdinalIgnoreCase))
+            {
+                return Language.Xml;
+            }
+
+            if (string.Equals(language, "xaml", StringComparison.OrdinalIgnoreCase))
+            {
+                return Language.Xml;
+            }
+
             return Language.Unknown;
         }
 
         private void AddCodeBlock(string text, string language)
         {
-            var codeBlock = new CodeBlock() { Language = ParseLanguage(language), Text = text.Trim(newlineChars) };
-            doc.Blocks.Add(codeBlock);
+            var codeBlock = new CodeBlock { Language = this.ParseLanguage(language), Text = text.Trim(newlineChars) };
+            this.doc.Blocks.Add(codeBlock);
         }
-
-        private Regex NewlineWhitespaceExpression = new Regex(@"\s*\r?\n\r?\s*", RegexOptions.Compiled);
 
         private void AddList(string value)
         {
             List list = null;
-            var matches = ListItemExpression.Matches(value);
+            var matches = this.ListItemExpression.Matches(value);
             foreach (Match match in matches)
             {
                 var level = match.Groups[1] != null ? match.Groups[1].Length : 0;
@@ -123,11 +203,14 @@ namespace LynxToolkit.Documents
                 int end = value.Length;
                 var next = match.NextMatch();
                 if (next.Success)
+                {
                     end = next.Index;
+                }
+
                 var start = match.Index + match.Length;
                 var item = value.Substring(start, end - start).Trim();
                 var li = new ListItem();
-                ParseInlines(item, li.Content);
+                this.ParseInlines(item, li.Content);
                 if (list == null)
                 {
                     if (unordered)
@@ -139,18 +222,19 @@ namespace LynxToolkit.Documents
                         list = new OrderedList();
                     }
 
-                    doc.Blocks.Add(list);
+                    this.doc.Blocks.Add(list);
                 }
+
                 list.Items.Add(li);
             }
         }
 
         private void AddQuote(string text)
         {
-            text = text.Replace("> ", "");
+            text = text.Replace("> ", string.Empty);
             var quote = new Quote();
-            ParseInlines(text, quote.Content);
-            doc.Blocks.Add(quote);
+            this.ParseInlines(text, quote.Content);
+            this.doc.Blocks.Add(quote);
         }
 
         private void AddTable(string text)
@@ -160,7 +244,7 @@ namespace LynxToolkit.Documents
             foreach (var line in lines)
             {
                 var tr = new TableRow();
-                foreach (Match match in TableRowExpression.Matches(line))
+                foreach (Match match in this.TableRowExpression.Matches(line))
                 {
                     TableCell td = match.Groups[1].Value == "|" ? new TableCell() : new TableHeaderCell();
                     var content = match.Groups[2].Value;
@@ -168,39 +252,34 @@ namespace LynxToolkit.Documents
                     var right = content.StartsWith("  ");
                     var left = content.EndsWith("  ");
                     if (left && right)
+                    {
                         td.HorizontalAlignment = HorizontalAlignment.Center;
-                    if (right && !left)
-                        td.HorizontalAlignment = HorizontalAlignment.Right;
+                    }
 
-                    ParseInlines(content.Trim(), td.Content);
+                    if (right && !left)
+                    {
+                        td.HorizontalAlignment = HorizontalAlignment.Right;
+                    }
+
+                    this.ParseInlines(content.Trim(), td.Content);
                     tr.Cells.Add(td);
                 }
+
                 table.Rows.Add(tr);
             }
 
-            doc.Blocks.Add(table);
+            this.doc.Blocks.Add(table);
         }
 
-        protected virtual void AddHeader(string text, int level)
+        private string Decode(string input)
         {
-            var h = new Header { Level = level };
-            ParseInlines(text, h.Content);
-            doc.Blocks.Add(h);
+            return CharacterReplacements.Decode(input);
         }
-
-        protected virtual void AddParagraph(string text)
-        {
-            var pa = new Paragraph();
-            ParseInlines(text, pa.Content);
-            doc.Blocks.Add(pa);
-        }
-
-        private static char[] newlineChars = new char[] { '\r', '\n' };
 
         private void ParseInlines(string text, IList<Inline> inlines)
         {
             text = text.Trim(newlineChars);
-            var matches = InlineExpression.Matches(text);
+            var matches = this.InlineExpression.Matches(text);
 
             int index = 0;
             foreach (Match match in matches)
@@ -218,16 +297,16 @@ namespace LynxToolkit.Documents
                 {
                     var innertext = strongGroup.Value;
                     var strong = new Strong();
-                    ParseInlines(innertext, strong.Content);
+                    this.ParseInlines(innertext, strong.Content);
                     inlines.Add(strong);
                 }
 
-                var emGroup = match.Groups["em"];
-                if (emGroup.Success)
+                var emphasizedGroup = match.Groups["em"];
+                if (emphasizedGroup.Success)
                 {
-                    var innertext = emGroup.Value;
+                    var innertext = emphasizedGroup.Value;
                     var em = new Emphasized();
-                    ParseInlines(innertext, em.Content);
+                    this.ParseInlines(innertext, em.Content);
                     inlines.Add(em);
                 }
 
@@ -261,17 +340,19 @@ namespace LynxToolkit.Documents
                     inlines.Add(a);
                 }
 
-                var aGroup = match.Groups["ahref"];
-                if (aGroup.Success)
+                var linkGroup = match.Groups["ahref"];
+                if (linkGroup.Success)
                 {
                     var innertext = match.Groups["atext"].Value;
                     var url = match.Groups["ahref"].Value;
                     var title = match.Groups["atitle"].Value;
                     if (string.IsNullOrWhiteSpace(innertext))
-                        innertext = url.Replace("http://", "");
+                    {
+                        innertext = url.Replace("http://", string.Empty);
+                    }
 
                     var a = new Hyperlink { Url = url, Title = title };
-                    ParseInlines(innertext, a.Content);
+                    this.ParseInlines(innertext, a.Content);
                     inlines.Add(a);
                 }
 
@@ -281,18 +362,24 @@ namespace LynxToolkit.Documents
                     var src = match.Groups["imgsrc"].Value;
                     var alt = match.Groups["imgalt"].Value;
                     var imglink = match.Groups["imglink"].Value;
-                    var img = new Image { Source = src, BaseDirectory = this.BaseDirectory, AlternateText = alt, Link = imglink };
+                    var img = new Image
+                                  {
+                                      Source = src, 
+                                      BaseDirectory = this.BaseDirectory, 
+                                      AlternateText = alt, 
+                                      Link = imglink
+                                  };
                     inlines.Add(img);
                 }
 
-                var brGroup = match.Groups["br"];
-                if (brGroup.Success)
+                var linebreakGroup = match.Groups["br"];
+                if (linebreakGroup.Success)
                 {
                     inlines.Add(new LineBreak());
                 }
 
-                var nGroup = match.Groups["n"];
-                if (nGroup.Success)
+                var newlineGroup = match.Groups["n"];
+                if (newlineGroup.Success)
                 {
                     inlines.Add(new Run(" "));
                 }
@@ -307,13 +394,8 @@ namespace LynxToolkit.Documents
 
         private void ParseRun(string text, IList<Inline> inlines)
         {
-            var s = this.Decode(text.Trim());
+            var s = this.Decode(text);
             inlines.Add(new Run(s));
-        }
-
-        private string Decode(string input)
-        {
-            return CharacterReplacements.Decode(input);
         }
     }
 }
