7bbcd7730a7c31a71985dd46e3da79fc7ddf8138
Oystein Bjorke <oystein.bjorke@gmail.com>
2013-10-16 07:44:47 +0200

    GlobalAssemblyInfo: removed ComVisible attribute

--- Source/GlobalAssemblyInfo.cs
+++ Source/GlobalAssemblyInfo.cs
@@ -25,13 +25,11 @@
 // </copyright>
 // --------------------------------------------------------------------------------------------------------------------
 using System.Reflection;
-using System.Runtime.InteropServices;
 
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("LynxToolkit")]
 [assembly: AssemblyCopyright("Copyright (c) LynxToolkit 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: ComVisible(false)]
 [assembly: AssemblyVersion("2013.1.1.*")]
 [assembly: AssemblyFileVersion("2013.1.1.0")]
