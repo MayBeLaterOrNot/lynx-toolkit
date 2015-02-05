1cc604e3ba15b0d8a5fedb92d923aa56b57d3713
unknown <objo@OSLLP130753.verit.dnv.com>
2015-02-05 09:38:39 +0100

    Add extract text tool

---
+++ Source/ExtractText/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace ExtractText
+{
+    using System.IO;
+    using System.Text.RegularExpressions;
+
+    class Program
+    {
+        private const string DefaultPattern = @"\n\#\#.*?\n(.*?)\n\#\#[^#]";
+
+        static void Main(string[] args)
+        {
+            var inputFile = args[0];
+            var outputFile = args[1];
+            var pattern = args.Length > 2 ? args[2] : DefaultPattern;
+            var input = File.ReadAllText(inputFile);
+            var match = Regex.Match(input, pattern, RegexOptions.Singleline);
+            if (match.Success)
+            {
+                File.WriteAllText(outputFile, match.Groups[1].Value.Trim());
+            }
+        }
+    }
+}
---
+++ Source/ExtractText/README.md
@@ -0,0 +1,11 @@
+﻿Finds the first match of the specified pattern and writes the value of the first group to the output file.
+
+## Syntax
+
+```
+ExtractText.exe <input-file> <output-file> [pattern]
+```
+
+input-file   The input file (required).
+output-file  The output file (required).
+pattern      The regular expression (optional). The default pattern is `"\n\#\#.*?\n(.*?)\n\#\#[^#]"`
