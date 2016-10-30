a0d3252efef359bb141f5327031693b5164ab267
Oystein Bjorke <oystein.bjorke@gmail.com>
2016-10-30 08:29:02 +0100

    remove obsolete files

--- Build/CleanSource.cmd
+++
@@ -1,2 +0,0 @@
-..\Output\CleanSource.exe /cleansummary /cleanregions ..\Source > CleanSource.log
-..\Output\UpdateFileHeaders.exe "/company=Lynx Toolkit" /copyright-file=..\License.txt ..\Source > UpdateFileHeaders.log
